5e3bd47346051ce74e7b2aba166c11f29ecb4873
LuizFernando2-prog <69861259+LuizFernando2-prog@users.noreply.github.com>
2023-03-30 17:01:26 -0300

    Add files via upload

---
+++ Teste-Adisra-Carros/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Carro
+{
+    public int NumeroChassi { get; set; }
+    public int NumeroMotor { get; set; }
+    public double CustoProducao { get; set; }
+
+    public virtual double CalcularImposto()
+    {
+        return CustoProducao * 0.2;
+    }
+
+    public virtual double CalcularCustoVenda()
+    {
+        double imposto = CalcularImposto();
+        double lucro = CustoProducao * 0.25;
+        return CustoProducao + imposto + lucro;
+    }
+}
+
+class CarroFlex : Carro
+{
+    public int NumeroPortas { get; set; }
+    public double Cilindrada { get; set; }
+
+    public override double CalcularCustoVenda()
+    {
+        double imposto = CalcularImposto();
+        double lucro = CustoProducao * 0.25;
+        return CustoProducao + imposto + lucro;
+    }
+}
+
+class CarroDiesel : Carro
+{
+    public double CapacidadeCarga { get; set; }
+    public double VolumeCacamba { get; set; }
+
+    public override double CalcularImposto()
+    {
+        return CustoProducao * 0.3;
+    }
+
+    public override double CalcularCustoVenda()
+    {
+        double imposto = CalcularImposto();
+        double lucro = CustoProducao * 0.25;
+        return CustoProducao + imposto + lucro;
+    }
+}
+
+class CarroEletrico : Carro
+{
+    public double Potencia { get; set; }
+    public double DuracaoBateria { get; set; }
+
+    public override double CalcularImposto()
+    {
+        return CustoProducao * 0.1;
+    }
+
+    public override double CalcularCustoVenda()
+    {
+        double imposto = CalcularImposto();
+        double lucro = CustoProducao * 0.25;
+        return CustoProducao + imposto + lucro;
+    }
+}
